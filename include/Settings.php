e725603bb577899f7ef81977e2774081ba0c06a0
linbil24 <linbilcelestre3@gmail.com>
2026-04-20 09:47:08 +0800

    .

--- include/Settings.php
+++ include/Settings.php
@@ -362,7 +362,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <title>Account Settings - Admin</title>
     <link rel="icon" type="image/x-icon" href="../assets/image/logo2.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="../assets/css/facilities-reservation.css?v=11">
+    <link rel="stylesheet" href="../assets/css/facilities-reservation.css?v=12">
     <style>
         :root {
             --primary-blue: #1e3a8a;
